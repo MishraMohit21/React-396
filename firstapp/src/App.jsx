62ae66458ee98bf4e9ee9708cc9005240d65acb5
MishraMohit21 <mishraloopmohit@gmail.com>
2025-01-28 13:40:56 +0530

    Console Task

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const data = await response.json();
+        console.log(data);
+        setUsers(data);
+      } catch (e) {
+        console.error("Error fetching user data:", e);
+      }
+    };
+
+    fetchUserData();
+  }, []); 
+
   return (
     <div>
       <h1>Roll Number - 1/22/FET/BCS/396</h1>
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
